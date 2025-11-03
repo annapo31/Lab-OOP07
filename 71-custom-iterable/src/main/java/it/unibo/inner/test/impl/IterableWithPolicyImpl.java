881fe0914d8a9a9e478434f56b43bf2ab53f6a25
Anna Pollini - anna.pollini2@studio.unibo.it <anna.pollini2@studio.unibo.it>
2025-11-03 13:44:27 +0100

    Solved the second part of the exercise

--- 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/test/impl/IterableWithPolicyImpl.java
@@ -10,6 +10,7 @@ import java.util.Iterator;
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
 
     private List<T> list;
+    private Predicate<T> filter;
 
     /**
      * Constructor of the class. It creates a new object of IterableWithPolicyImpl
@@ -17,17 +18,31 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
      * @param elements array with the elements to set our list
      */
     public IterableWithPolicyImpl (final T[] elements){
+        // Si fa con classe anonima che ritorna sempre un true
+        this(elements, new Predicate<T>() {
+                public boolean test(T elem) { return true; }
+            });
+    }
+
+    /** 
+     * Constructor of the class. It creates a new object of IterableWithPolicyImpl
+     * 
+     * @param elements array with the elements to set our list
+     * @param filter will be used to filter the elements during the iteration
+     */
+    public IterableWithPolicyImpl (final T[] elements, final Predicate<T> filter){
         List<T> list = List.of(elements);
 
         /*for(int i = 0; i < elements.length; i++) {
             array_list.add(i, elements[i]);
         }*/
-
         this.list = list;
+        this.filter = filter;
     }
 
     @Override
     public void setIterationPolicy(Predicate<T> filter){
+        this.filter = filter;
     }
 
     public Iterator<T> iterator() {
@@ -38,10 +53,12 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
     class IteratorImpl implements Iterator<T> {
 
         private int position = 0;
+        // Bisogna fare un ciclo
 
+        /* previous implementation
         @Override
         public boolean hasNext(){
-           return (position < list.size());
+            return (position < list.size());
         }
 
         @Override
@@ -49,7 +66,27 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T>{
             if(hasNext()) {
                 return list.get(position++);
             }
-            throw new NoSuchElementException("You're reached the end of the list");
+            throw new NoSuchElementException("You've reached the end of the list");
+        }*/
+
+        @Override
+        public boolean hasNext(){
+            boolean succeded = false;
+            while(position < list.size() && succeded == false){
+                succeded = filter.test(list.get(position));
+                if(!succeded) {
+                    position++;
+                }
+            }
+            return succeded;
+        }
+
+        @Override
+        public T next() {
+            if(hasNext()) {
+                return list.get(position++);
+            }
+            throw new NoSuchElementException("You've reached the end of the list");
         }
     }
 }
